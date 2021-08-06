e3bd7eeff104e11cf529875a18810cb9bcec4221
1095450045 <1095450045@qq.com>
2021-08-06 13:48:06 +0800

    测试项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <h2>父组件的值:{{fathermsg}}</h2>
+    <input type="text" v-model="fathermsg">
+    <Son :msg="fathermsg" v-on:smsg="smsgs"></Son>
+  </div>
+</template>
+
+<script>
+import Son from './son.vue'
+export default {
+  
+  data () {
+    return {
+      fathermsg:0
+    }
+  },
+  components:{
+    Son
+  },
+  methods:{
+    smsgs(e){
+      console.log(e)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/son.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <h2>
+          子组件的值:{{msg}}
+      </h2>
+      <input type="text" v-model="msg" @change="setdata">
+  </div>
+</template>
+
+<script>
+export default {
+    props:['msg'],
+    methods:{
+        setdata(){
+            this.$emit('smsg',this.msg)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
